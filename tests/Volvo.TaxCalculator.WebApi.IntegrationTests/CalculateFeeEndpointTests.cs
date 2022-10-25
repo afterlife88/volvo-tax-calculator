916f0dd190df94932c86d95770f859cd61f665ed
Andrii Iasko <andrii.iasko@gmail.com>
2022-10-26 00:16:08 +0200

    Happy path integration test

--- tests/Volvo.TaxCalculator.WebApi.IntegrationTests/CalculateFeeEndpointTests.cs
+++ tests/Volvo.TaxCalculator.WebApi.IntegrationTests/CalculateFeeEndpointTests.cs
@@ -7,6 +7,28 @@ namespace Volvo.TaxCalculator.WebApi.IntegrationTests;
 
 public sealed class CalculateFeeEndpointTests
 {
+    private const string ValidCity = "Gothenburg";
+    private const string EndpointPath = "/calculate-fee";
+
+    [Fact]
+    public async Task ItCalculatesFeeByTheRules()
+    {
+        var webAppFactory = new WebApplicationFactory<Program>();
+        var httpClient = webAppFactory.CreateClient();
+        var result = await httpClient.PostAsJsonAsync(EndpointPath,
+            new CalculateTaxRequest
+            (ValidCity,
+                VehicleType.Car,
+                new DateTime[]
+                {
+                    new(2013, 2, 8, 6, 20, 0),
+                    new(2013, 2, 8, 6, 27, 0),
+                }));
+
+        Assert.Equal((HttpStatusCode)200, result.StatusCode);
+        Assert.Equal("8", result.Content.ReadAsStringAsync().Result);
+    }
+
     [Theory]
     [InlineData("Gothenburg", HttpStatusCode.OK)]
     [InlineData("Copenhagen", HttpStatusCode.UnprocessableEntity)]
@@ -16,7 +38,7 @@ public sealed class CalculateFeeEndpointTests
     {
         var webAppFactory = new WebApplicationFactory<Program>();
         var httpClient = webAppFactory.CreateClient();
-        var result = await httpClient.PostAsJsonAsync("/calculate-fee",
+        var result = await httpClient.PostAsJsonAsync(EndpointPath,
             new CalculateTaxRequest
             (cityName,
                 VehicleType.Car,
