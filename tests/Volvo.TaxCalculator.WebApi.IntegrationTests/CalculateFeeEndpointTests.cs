5982ad19e66527efcf69a11539c9d5361e1e897d
Andrii Iasko <andrii.iasko@gmail.com>
2022-10-26 00:02:37 +0200

    Added integration tests

---
+++ tests/Volvo.TaxCalculator.WebApi.IntegrationTests/CalculateFeeEndpointTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Volvo.TaxCalculator.WebApi.IntegrationTests;
+
+public sealed class CalculateFeeEndpointTests
+{
+    [Theory]
+    [InlineData("Gothenburg", HttpStatusCode.OK)]
+    [InlineData("Copenhagen", HttpStatusCode.UnprocessableEntity)]
+    [InlineData("", HttpStatusCode.BadRequest)]
+    [InlineData(default, HttpStatusCode.BadRequest)]
+    public async Task ItCalculatesFeeForValidCity(string cityName, HttpStatusCode expectedStatusCode)
+    {
+        var webAppFactory = new WebApplicationFactory<Program>();
+        var httpClient = webAppFactory.CreateClient();
+        var result = await httpClient.PostAsJsonAsync("/calculate-fee",
+            new CalculateTaxRequest
+            (cityName,
+                VehicleType.Car,
+                new[] { DateTime.Now }));
+
+        Assert.Equal(expectedStatusCode, result.StatusCode);
+    }
+}
